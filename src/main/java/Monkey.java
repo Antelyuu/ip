e84795d8721261a7900838eb3fad036d332f9763
Antelyuu <e1525098@u.nus.edu>
2026-08-19 22:44:28 +0800

    Added Greeting

--- src/main/java/Monkey.java
+++ src/main/java/Monkey.java
@@ -1,11 +1,19 @@
 public class Monkey {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = " __  __              _\n"
                 + "|  \\/  | ___  _ __  | | _____ _   _\n"
                 + "| |\\/| |/ _ \\| '_ \\ | |/ / _ \\ | | |\n"
                 + "| |  | | (_) | | | ||   <  __/ |_| |\n"
                 + "|_|  |_|\\___/|_| |_||_|\\_\\___|\\__, |\n"
                 + "                              |___/\n";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Monkey.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
